780449d06981e7a6f81832ac609bbc405eb10b5b
Pablo Godoy <pagodoy@telecentro.net.ar>
2021-12-28 20:16:50 -0300

    completed 3 test cases

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-function Error() {
-	return <div data-testid="errorMsg" className="alert error mt-20 slide-up-fade-in">Error Message</div>
+function Error({ msg = '' }) {
+	return (
+		<div data-testid="errorMsg" className="alert error mt-20 slide-up-fade-in">
+			{msg}
+		</div>
+	);
 }
 
 export default Error;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { STUDENTS } from '../studentsList';
 // `joiningDate` && `validityDate` format "yyyy-mm-dd"
 
 function checkValidity(joiningDate, validityDate) {
@@ -9,24 +9,101 @@ function checkValidity(joiningDate, validityDate) {
 	const [yyyy, mm, dd] = validityDate.split('-');
 	const maxValid = new Date(yyyy, mm - 1, dd);
 	const selected = new Date(year, month - 1, day);
-	return (maxValid >= selected) && (maxValid >= today);
+	return maxValid >= selected && maxValid >= today;
 }
 
-function Search() {
+function Search({ addToResident, notifyError }) {
+	const [name, setName] = React.useState('');
+	const [date, setDate] = React.useState('');
+	const [isDisabled, setIsDisabled] = React.useState(true);
+
+	const handleChange = (e, type) => {
+		e.preventDefault();
+		if (type && type === 'text') {
+			setName(e.target.value);
+		}
+
+		if (type && type === 'date') {
+			setDate(e.target.value);
+		}
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		const [filteredStudent] = STUDENTS.filter(
+			(est) => est.name.toLowerCase() === name.toLowerCase()
+		);
+
+		let studentValidated;
+
+		if (filteredStudent?.validityDate && date) {
+			studentValidated = checkValidity(date, filteredStudent?.validityDate);
+		}
+
+		if (!filteredStudent?.length) {
+			notifyError(`Sorry, ${name}'s validity has Expired!`);
+		}
+
+		if (!studentValidated) {
+			notifyError(`Sorry, ${name}'s is not a verified student!`);
+		}
+
+		if (filteredStudent && studentValidated) {
+			addToResident(filteredStudent);
+		}
+
+		setName('');
+		setDate('');
+	};
+
+	// console.log(STUDENTS);
+
+	React.useEffect(() => {
+		if (name && date) {
+			setIsDisabled(false);
+		}
+	}, [name, date]);
+
 	return (
-		<div className="my-50 layout-row align-items-end justify-content-end">
-			<label htmlFor="studentName">Student Name:
+		<form
+			onSubmit={submitHandler}
+			className="my-50 layout-row align-items-end justify-content-end"
+		>
+			<label htmlFor="studentName">
+				Student Name:
 				<div>
-					<input id="studentName" data-testid="studentName" type="text" className="mr-30 mt-10"/>
+					<input
+						id="studentName"
+						data-testid="studentName"
+						type="text"
+						className="mr-30 mt-10"
+						value={name}
+						onChange={(e) => handleChange(e, 'text')}
+					/>
 				</div>
 			</label>
-			<label htmlFor="joiningDate">Joining Date:
+			<label htmlFor="joiningDate">
+				Joining Date:
 				<div>
-					<input id="joiningDate" data-testid="joiningDate" type="date" className="mr-30 mt-10"/>
+					<input
+						id="joiningDate"
+						data-testid="joiningDate"
+						type="date"
+						value={date}
+						className="mr-30 mt-10"
+						onChange={(e) => handleChange(e, 'date')}
+					/>
 				</div>
 			</label>
-			<button type="button" data-testid="addBtn" className="small mb-0">Add</button>
-		</div>
+			<button
+				disabled={isDisabled}
+				type="submit"
+				data-testid="addBtn"
+				className="small mb-0"
+			>
+				Add
+			</button>
+		</form>
 	);
 }
 
